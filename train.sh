4d08c3b09bc0ecdacdaf05a832e96d1794d0c1a4
zpp2 <zpp2@zpp2-System-Product-Name>
2024-04-24 20:40:07 +0800

    UPDATE

--- train.sh
+++ train.sh
@@ -1,5 +1,5 @@
 export DETECTRON2_DATASETS='/home/zpp2/ycy/datasets/'
-# export CUDA_VISIBLE_DEVICES=1
+export CUDA_VISIBLE_DEVICES=1
 python train_net.py \
   --config-file configs/coco/panoptic-segmentation/fcclip/fcclip_convnext_large_eval_landdiscover50k.yaml \
-  --num-gpus 2 SOLVER.IMS_PER_BATCH 10
+  --num-gpus 1 SOLVER.IMS_PER_BATCH 4
