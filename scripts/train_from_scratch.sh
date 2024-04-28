b232037894262353b7a9f25a050f439ee3e7e03c
zpp2 <zpp2@zpp2-System-Product-Name>
2024-04-28 15:50:07 +0800

    renew positional encoding

--- scripts/train_from_scratch.sh
+++ scripts/train_from_scratch.sh
@@ -4,3 +4,4 @@ python train_net.py \
   --config-file configs/coco/panoptic-segmentation/fcclip/fcclip_convnext_large_eval_landdiscover50k.yaml \
   --num-gpus 2 SOLVER.IMS_PER_BATCH 8 SOLVER.MAX_ITER 100000 DATALOADER.NUM_WORKERS 16
   
+
--- scripts/train_ft.sh
+++ scripts/train_ft.sh
@@ -1,7 +1,7 @@
 export DETECTRON2_DATASETS='/home/zpp2/ycy/datasets/'
 export CUDA_VISIBLE_DEVICES=1,0
 python train_net.py \
-  --config-file configs/coco/panoptic-segmentation/fcclip/fcclip_convnext_large_eval_landdiscover50k.yaml \
-  --num-gpus 2 --resume True \
+  --config-file "configs/coco/panoptic-segmentation/fcclip/LandDiscover50K_fcclip_convnext_large_eval_FLAIR&Potsdam&FloodNet&FAST.yaml" \
+  --num-gpus 2 --resume \
   OUTPUT_DIR /media/zpp2/PHDD/FC-CLIP-output/output_FT_100K_Batch8 MODEL.WEIGHTS fcclip_cocopan.pth SOLVER.IMS_PER_BATCH 8 SOLVER.MAX_ITER 100000 DATALOADER.NUM_WORKERS 16 \
   
